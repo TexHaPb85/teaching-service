4f11f181f0ca78dcdbfe67747ff11a696456fa15
myevtushenko <16fi.m.yevtushenko@std.npu.edu.ua>
2022-06-23 13:16:18 +0300

    db changed

--- src/main/resources/db/migration/V6__FillTaskTables1.sql
+++ src/main/resources/db/migration/V6__FillTaskTables1.sql
@@ -1,8 +1,8 @@
 insert into task_materials (task_id, question, answer, task_type, complexity_value, paren_topic_id)
 values ('data_types_basic1',
-        '<p>What will be printed to console?</p>
-        <p>int a = 10;</p>
-        <p>System.out.println(++a);</p>
+        '<pre>What will be printed to console?
+        int a = 10;
+        System.out.println(++a);</pre>
         ',
         '11',
         'QUESTION',
@@ -11,13 +11,13 @@ values ('data_types_basic1',
 
 insert into task_materials (task_id, question, answer, task_type, complexity_value, paren_topic_id)
 values ('data_types_basic2',
-        '<p>Write code, that will increase integer variable "a" by 1 and return it.</p>
-         <p>Fill the following code:</p>' ||
+        '<pre>Write code, that will increase integer variable "a" by 1 and return it as a String.
+         Fill the following code:' ||
         '{
 int a = 0;
 // write your code here
 return String.valueOf(a);
-}1;
+}</pre>;
         ',
         '1',
         'WRITE_CODE',
@@ -26,27 +26,29 @@ return String.valueOf(a);
 
 insert into task_materials (task_id, question, answer, task_type, complexity_value, paren_topic_id)
 values ('data_types_middle1',
-        '<p>What will be printed to console?
-        int a = 10;
-        int b = a--;
-        int c = --b;
-        b*=5;
-        System.out.println("a=" + (++a) + ", b=" + b + ", c=" + c);</p>
+        '<p>
+        What will be printed to console?</p>
+        <p>int a = 10;</p>
+        <p>int b = a--;</p>
+        <p>int c = --b;</p>
+        <p>b*=5;</p>
+        <p>System.out.println("a=" + (++a) + ", b=" + b + ", c=" + c);
+        </p>
         ',
         'a=10, b=45, c=9',
         'QUESTION',
-        0.4,
+        0.39,
         'data_types');
 
 insert into task_materials (task_id, question, answer, task_type, complexity_value, paren_topic_id)
 values ('data_types_middle2',
-        '<p>What will be printed to console?
-        int a = 10;
-        int b = --a;
-        int c = --b;
-        c%=--a;
-        System.out.println("a=" + a + ", b=" + b + ", c=" + c);</p>
-        ',
+'<pre>What will be printed to console?
+int a = 10;
+int b = --a;
+int c = --b;
+c%=--a;
+System.out.println("a=" + a + ", b=" + b + ", c=" + c);</pre>
+',
         'a=8, b=8, c=0',
         'QUESTION',
         0.4,
@@ -54,15 +56,15 @@ values ('data_types_middle2',
 
 insert into task_materials (task_id, question, answer, task_type, complexity_value, paren_topic_id)
 values ('data_types_middle3',
-        '<p>Write code, that will increase integer variable "a" by 15 and return it.</p>
-         <p>Fill the following code:</p>' ||
+        '<p>Write code, that will increase integer variable "a" by 15 and return it as String.</p>
+         <pre>Fill the following code:' ||
         '{
 int a = 0;
 // write your code here
 return String.valueOf(a);
-}1;
+}</pre>;
         ',
         '15',
         'WRITE_CODE',
-        0.4,
+        0.38,
         'data_types');
